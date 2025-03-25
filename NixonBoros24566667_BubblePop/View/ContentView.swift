aa59e85af008929f211238e92a7d2d9883be385a
nixon <126039614+nixonboros@users.noreply.github.com>
2025-03-25 14:47:36 +1100

    Basic MainMenu screen

--- NixonBoros24566667_BubblePop/View/ContentView.swift
+++ NixonBoros24566667_BubblePop/View/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerName: String = ""
+    @State private var isGameStarted = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isGameStarted {
+                GameView(playerName: playerName)
+            } else {
+                // Main Menu Screen
+                TextField("Enter Name", text: $playerName)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(5)
+                    .padding(.horizontal)
+                
+                Button(action: {
+                    startGame()
+                }) {
+                    Text("Start Game")
+                        .font(.title)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
         }
-        .padding()
+    }
+    
+    func startGame() {
+        // Validate name entry
+        if playerName.isEmpty {
+            playerName = "Player" // Use a default name if empty
+        }
+        
+        isGameStarted = true
     }
 }
 
---
+++ NixonBoros24566667_BubblePop/View/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  NixonBoros24566667_BubblePop
+//
+//  Created by Nixon Boros on 25/3/2025.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    let playerName: String
+    
+    var body: some View {
+        Text(playerName)
+    }
+}
+
+#Preview {
+    GameView(playerName: "Player")
+}
